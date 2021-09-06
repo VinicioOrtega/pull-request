4015283d3c304bf1079c49d7ceec0950cc776802
VinicioOrtega <49877989+VinicioOrtega@users.noreply.github.com>
2021-09-06 16:40:22 -0500

    Update mike.vue

--- pages/mike.vue
+++ pages/mike.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="12" md="12">
+    <v-col cols="14" sm="14" md="14">
       <v-footer
         color="primary lighten-1"
         padless
